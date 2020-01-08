c2be387453e453feb230fb23d4c4b994dc43274a
silascaimi <silascaimi@live.com>
2020-01-08 19:22:45 -0300

    Adicione a funcao de excluir item da lista

--- js/todos.js
+++ js/todos.js
@@ -15,18 +15,34 @@ function renderTodos(){
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
 
+        var linkElement = document.createElement('a');
+
+        linkElement.setAttribute('href','#');
+
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick','deleteTodo('+pos+')');
+
+        var linkText = document.createTextNode('Excluir');
+
+        linkElement.appendChild(linkText);
+
         todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+
         listElement.appendChild(todoElement);
     }
 }
 
 renderTodos();
 
-function addTodo(){
+buttonElement.onclick = function addTodo(){
     var todoText = inputElement.value;
 
     todos.push(todoText);
     renderTodos();
 }
 
-buttonElement.onclick = addTodo;
+function deleteTodo(pos){
+    todos.splice(pos, 1);
+    renderTodos();
+}
