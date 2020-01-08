1c77a3017bb230468171d88878d21f7586405ca6
silascaimi <silascaimi@live.com>
2020-01-08 17:49:49 -0300

    Initial Commit
    Adiciona os arquivos index.html e js/todos.js

---
+++ js/todos.js
@@ -0,0 +1,9 @@
+var listElement = document.querySelector('#app ul');
+var inputElemet = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var todos = [
+    'Fazer café',
+    'Estudar JavaScript',
+    'Acessar comunidade'
+];
